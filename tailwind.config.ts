f23e2445f04921caaa7237c66febcd241e3bbcb2
KALLOL BHATTACHARJEE <kallolbhattacharjee.2004@gmail.com>
2025-08-11 12:18:41 +0530

    Search form

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
 			colors: {
 				primary: {
 					"100": "#FFE8F0",
-					DEFAULT: "#EE2B69",
+					DEFAULT: "#f67d20ff",
 				},
 				secondary: "#FBE843",
 				black: {
